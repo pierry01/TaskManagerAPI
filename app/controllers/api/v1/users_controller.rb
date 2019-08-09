25b0a7f32c9043ed71b3af7ee136aef11c60dff9
pierry01 <pierrybm@gmail.com>
2019-08-09 16:16:40 +0000

    Added update action to users controller with tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::UsersController < ApplicationController
   
   def show
     begin
-      @user = User.find(params[:id])
-      respond_with @user
+      user = User.find(params[:id])
+      respond_with user
     rescue
       head 404
     end
@@ -15,6 +15,16 @@ class Api::V1::UsersController < ApplicationController
     
     if user.save
       render json: user, status: 201
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+  
+  def update
+    user = User.find(params[:id])
+    
+    if user.update(user_params)
+      render json: user, status: 200
     else
       render json: { errors: user.errors }, status: 422
     end
