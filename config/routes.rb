6797ffcd2f89cbb405036640e9ea6a1c23b00c09
pierry01 <pierrybm@gmail.com>
2019-08-13 01:21:47 +0000

    Adding create action to the sessions controller with tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 require 'api_version_constraint'
 
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users, only: [:sessions], controllers: { sessions: 'api/v1/sessions' }
   
   namespace :api, defaults: { format: :json } do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       resources :users, only: [:show, :create, :update, :destroy]
+      resources :sessions, only: [:create]
     end
   end
 end
