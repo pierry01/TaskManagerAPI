392086594dce22672c8999a6edfaf78e146c1b9f
pierry01 <pierrybm@gmail.com>
2019-08-14 18:03:52 +0000

    Adding API::V2

---
+++ spec/requests/api/v2/sessions_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe 'Sessions API', type: :request do
+  let(:user) { create(:user) }
+  
+  let(:headers) do
+    {
+      'Accept' => 'application/vnd.taskmanager.v2',
+      'Content-Type' => Mime[:json].to_s
+    }
+  end
+  
+  describe 'POST /api/sessions' do
+    before { post '/api/sessions', params: { session: credentials }.to_json, headers: headers }
+  
+    context 'When the credentials are correct' do
+      let(:credentials) { { email: user.email, password: 123456 } }
+      
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+      
+      it 'returns JSON data for the user with auth token' do
+        user.reload
+        expect(json_body[:auth_token]).to eq(user.auth_token)
+      end
+    end
+    
+    context 'When the credentials are incorrect' do
+      let(:credentials) { { email: user.email, password: 'INVÁLIDO' } }
+      
+      it 'returns status code 401' do
+        expect(response).to have_http_status(401)
+      end
+      
+      it 'returns JSON data for the errors' do
+        expect(json_body).to have_key(:errors)
+      end
+    end
+  end
+  
+  describe 'DELETE /api/sessions/:id' do
+    let(:auth_token) { user.auth_token }
+    
+    before { delete "/api/sessions/#{auth_token}", params: {}, headers: headers }
+    
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+    
+    it 'changes the user auth_token' do
+      expect(User.find_by(auth_token: auth_token)).to be_nil
+    end
+  end
+end
---
+++ spec/requests/api/v2/tasks_spec.rb
@@ -0,0 +1,133 @@
+require 'rails_helper'
+
+RSpec.describe 'Tasks API', type: :request do
+  let!(:user) { create(:user) }
+  let(:headers) do
+    {
+      'Content-Type' => Mime[:json].to_s,
+      'Accept' => 'application/vnd.taskmanager.v2',
+      'Authorization' => user.auth_token
+    }
+  end
+  
+  describe 'GET /api/tasks' do
+    before do
+      create_list(:task, 5, user_id: user.id)
+      get '/api/tasks', params: {}, headers: headers
+    end
+    
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+    
+    it 'returns 5 tasks from database' do
+      expect(json_body[:tasks].count).to eq (5)
+    end
+  end
+  
+  describe 'GET /api/tasks/:id' do
+    let(:task) { create(:task, user_id: user.id) }
+    
+    before { get "/api/tasks/#{task.id}", params: {}, headers: headers }
+    
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns the json for task' do
+      expect(json_body[:title]).to eq(task.title)
+    end
+  end
+  
+  describe 'POST /api/tasks' do
+    before { post '/api/tasks', params: { task: task_params }.to_json, headers: headers }
+    
+    context 'when the request params are valid' do
+      let(:task_params) { attributes_for(:task) }
+      
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+      
+      it 'saves the task in the database' do
+        expect(Task.find_by(title: task_params[:title])).not_to be_nil
+      end
+      
+      it 'returns the json for created task' do
+        expect(json_body[:title]).to eq(task_params[:title])
+      end
+
+      it 'assigns the created task to the current user' do
+        expect(json_body[:user_id]).to eq(user.id)
+      end
+    end
+    
+    context 'when the request params are invalid' do
+      let(:task_params) { attributes_for(:task, title: nil) }
+      
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+      
+      it 'doest not save the task in the database' do
+        expect(Task.find_by(title: task_params[:title])).to be_nil
+      end
+
+      it 'returns the json errors for title' do
+        expect(json_body[:errors]).to have_key(:title)
+      end
+    end
+  end
+  
+  describe 'PUT /api/tasks/:id' do
+    let!(:task) { create(:task, user_id: user.id) }
+    
+    before { put "/api/tasks/#{task.id}", params: { task: task_params }.to_json, headers:headers }
+    
+    context 'when the params are valid' do
+      let(:task_params) { { title: 'New task title' } }
+      
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+      
+      it 'returns the json for updated task' do
+        expect(json_body[:title]).to eq(task_params[:title])
+      end
+      
+      it 'updates the task in the database' do
+        expect(Task.find_by(title: task_params[:title])).not_to be_nil
+      end
+    end
+    
+    context 'when the params are invalid' do
+      let(:task_params) { { title: nil } }
+      
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+      
+      it 'returns the json error for title' do
+        expect(json_body[:errors]).to have_key(:title)
+      end
+      
+      it 'does not updates the task in the database' do
+        expect(Task.find_by(title: task_params[:title])).to be_nil
+      end
+    end
+  end
+  
+  describe 'DELETE /api/tasks/:id' do
+    let!(:task) { create(:task, user_id: user.id) }
+    
+    before { delete "/api/tasks/#{task.id}", params: {}, headers: headers }
+    
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+    
+    it 'removes the task from the database' do
+      expect { Task.find(task.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
